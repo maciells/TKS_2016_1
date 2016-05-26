4794d02b59fb28afd1a21cd19bf4aa44ecf644c0
Jean <jean.ns24@hotmail.com>
2016-05-25 21:51:07 -0300

    Correção no menu. Adiciona aluno na turma.

--- ThekidsSchool_SA_2016_3_Semestre/src/br/senai/sc/thekidsschool/bean/TurmaMb.java
+++ ThekidsSchool_SA_2016_3_Semestre/src/br/senai/sc/thekidsschool/bean/TurmaMb.java
@@ -81,7 +81,7 @@ public class TurmaMb {
 	
 	public void excluirAluno(AjaxBehaviorEvent event) {
 		Usuario aluno = (Usuario) event.getComponent().getAttributes()
-				.get("idTurma");
+				.get("idAluno");
 		turma.getAlunosTurma().remove(aluno);
 	}
 
